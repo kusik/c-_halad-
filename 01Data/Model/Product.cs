e64d29e9b516b480cc8a112be96e9d01114aa555
Károly Kusinczky <kusik@windowslive.com>
2016-12-30 15:21:51 +0100

    Add project files.

---
+++ 01Data/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace _01Data.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
