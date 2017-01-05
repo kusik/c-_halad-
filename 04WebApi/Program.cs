e204ca59630c5faa11612f4b50502a5e7e198440
Kusik <kusinczky@gmail.com>
2017-01-05 14:56:56 +0100

    #1 : Nancy

---
+++ 04WebApi/Program.cs
@@ -0,0 +1,36 @@
+﻿using Nancy;
+using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04WebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://127.0.0.1:9000";
+
+            using (var host = new NancyHost(new Uri(url)))
+            {
+                host.Start();
+                Console.WriteLine("Elindult a Nancy szerver a {0} címen!",url);
+                Console.ReadLine();
+            }   
+
+        }
+    }
+    public class Utvalasztas : NancyModule
+    {
+
+        public Utvalasztas()
+        {
+            Get["/"] = _ => "Hello world, here is Nancy!";
+        }
+        
+    }
+
+}
