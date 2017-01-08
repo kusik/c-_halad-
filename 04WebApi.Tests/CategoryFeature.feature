da26ca03a065a00493bf7da4501ad40c0f2d7f8c
Károly Kusinczky <kusik@windowslive.com>
2017-01-08 20:01:22 +0100

    #1: WebApi tesztek

---
+++ 04WebApi.Tests/CategoryFeature.feature
@@ -0,0 +1,26 @@
+﻿Jellemző: Category kiszolgáló
+	fejlesztőként szükségem van egy webapi-ra, ami
+	kiszolgálja a Categóriákra vonatkozó kéréseket
+Forgatókönyv vázlat: WebApiCategoryGet
+	Adott egy böngésző
+	Ha meghívjuk ezt a címet: <url>
+	Akkor ezeket az adatokat kapjuk <id>,<name>
+Példák: 
+	| url          | id | name           |
+	| 'Category/1' | 1  | 'Processzorok' |
+	| 'Category/2' | 2  | 'Memóriák'     |
+	| 'Category/3' | 3  | 'Monitorok'    |
+
+Forgatókönyv vázlat: WebApiCategoryPost
+	Adott egy böngésző
+	Ha meghívjuk post-tal ezt a címet: <url> és átadjuk az értékeket : <key>, <value> 
+	Akkor hiba nélkül lefut a kérés
+Példák:
+	| url        | key    | value             |
+	| 'Category' | 'Name' | '1. új kategória' |
+	| 'Category' | 'Name' | '2. új kategória' |  
+	| 'Category' | 'Name' | '3. új kategória' |  
+	| 'Category' | 'Name' | '4. új kategória' |  
+
+
+
---
+++ 04WebApi.Tests/CategoryKiszolgaloSteps.cs
@@ -0,0 +1,56 @@
+﻿using _03Service;
+using Nancy;
+using Nancy.Testing;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace _04WebApi.Tests
+{
+    [Binding]
+    public class CategoryKiszolgaloSteps
+    {
+        private Browser browser;
+        private BrowserResponse responde;
+
+       
+
+        [Given(@"egy böngésző")]
+        public void AmennyibenEgyBongeszo()
+        {
+            browser = new Browser(with => with.Module(new Utvalasztas()));
+           
+        }
+        [When(@"meghívjuk ezt a címet: '(.*)'")]
+        public void MajdMeghivjukEztACimet(string url)
+        {
+            responde = browser.Get(url);
+        }
+
+        [Then(@"ezeket az adatokat kapjuk (.*),'(.*)'")]
+        public void AkkorEzeketAzAdatokatKapjuk(int id, string name)
+        {
+            var catVW = JsonConvert.DeserializeObject<CategoryViewModel>(responde.Body.AsString());
+            Assert.AreEqual(catVW.Id,id);
+            Assert.AreEqual(catVW.Name, name);
+        }
+         [When(@"meghívjuk post-tal ezt a címet: '(.*)' és átadjuk az értékeket : '(.*)', '(.*)'")]
+        public void MajdMeghivjukPOST_TalEztACimetEsAtadjukEzeketAzAdatokat(string url, string key, string value)
+        {
+            responde = browser.Post(url, with =>
+              {
+                  with.FormValue(key,value);
+              });
+        }
+
+        [Then(@"hiba nélkül lefut a kérés")]
+        public void AkkorHibaNelkulLefutAKeres()
+        {
+            Assert.AreEqual(HttpStatusCode.OK, responde.StatusCode);
+
+        }
+
+
+    }
+}
